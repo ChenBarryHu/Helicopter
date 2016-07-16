ca619792644d11d73a54ece845970fa1ea68b82d
Shichen Hu <hsctnt@gmail.com>
2016-07-16 12:35:47 -0400

    Correct the select bird function

--- app/src/main/java/com/shichen/android/helicopter/GameCharacter/BonusCommander.java
+++ app/src/main/java/com/shichen/android/helicopter/GameCharacter/BonusCommander.java
@@ -76,12 +76,12 @@ public class BonusCommander {
     private int selectBonus() {
         if(GamePanel.gravityInverseMode || GamePanel.unstoppableMode){ return 0;}
         double intermedia = random.nextDouble() * 100;
-        if (intermedia <= 50) {
-            return 2;
-        } else if(intermedia <= 90){
+        if (intermedia <= 80) {
             return 0;
-        } else {
+        } else if(intermedia <= 90){
             return 1;
+        } else {
+            return 2;
         }
     }
 
